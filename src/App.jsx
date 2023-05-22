9adb8788c8bf6d62e03ce7df134956090912a0f3
Jonathan Teheran <jonathanyyir@gmail.com>
2023-05-22 10:10:38 -0500

    Lista de tareas

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./componts/Header";
+import TaskList from "./componts/TaskList";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <TaskList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/componts/TaskList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Task from "./Task";
+
+const TaskList = () => {
+  const tasks = [
+    { id: 1, name: "task 1", completed: true },
+    { id: 2, name: "task 2", completed: false },
+    { id: 3, name: "task 3", completed: false },
+  ];
+
+  return (
+    <div>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
+    </div>
+  );
+};
+
+export default TaskList;
