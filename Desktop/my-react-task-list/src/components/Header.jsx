6ec895d4e929c1476043a53ea30fab55830aa2ed
Jonathan Teheran <jonathanyyir@gmail.com>
2023-07-14 12:31:39 -0500

    commit Forms

--- Desktop/my-react-task-list/src/components/Header.jsx
+++ Desktop/my-react-task-list/src/components/Header.jsx
@@ -3,19 +3,47 @@ import { BsPlus } from "react-icons/bs";
 import todoapp from "../assets/todoapp.png";
 import { useState } from "react";
 
+// eslint-disable-next-line react/prop-types
 function Header({ onAddTask }) {
   const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [formValidation, setFormValidation] = useState({
+    error: false,
+    errorMessage: "",
+  });
 
-  function handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
-    onAddTask(title);
-    setTitle("");
+    const error = title.length < 3;
+    setFormValidation({
+      error: error,
+      errorMessage: error
+        ? "La tarea debe contener por lo menos tres(3) caracteres"
+        : "",
+    });
+    if (!error) {
+      onAddTask(title, description);
+      setTitle("");
+      setDescription("");
+    }
+  };
+
+  function onKeyDown(event) {
+    if (event.key === "Enter") {
+      handleSubmit(event);
+    }
   }
 
-  function onChangeTitle(event) {
-    setTitle(event.target.value);
-  }
+  const onChangeTitle = (event) => {
+    const value = event.target.value;
+    setTitle(value);
+  };
+
+  const onChangeDescription = (event) => {
+    const value = event.target.value;
+    setDescription(value);
+  };
 
   return (
     <header className={styles.header}>
@@ -23,13 +51,28 @@ function Header({ onAddTask }) {
 
       <form onSubmit={handleSubmit} className={styles.newTaskForm}>
         <input
+          className={styles.inputTitle}
           placeholder="Agregar una nueva tarea"
           type="text"
           value={title}
           onChange={onChangeTitle}
         />
+
+        <input
+          className={styles.inputDescription}
+          placeholder="Agregar una descripción de tarea"
+          type="text"
+          value={description}
+          onChange={onChangeDescription}
+          onKeyDown={onKeyDown}
+        />
+
+        {formValidation.error ? (
+          <span className={styles.error}>{formValidation.errorMessage}</span>
+        ) : null}
         <button>
           <BsPlus size={25} />
+          <p>NEX</p>
         </button>
       </form>
     </header>
